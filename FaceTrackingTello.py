599820dd88417be07cdade7b876890960b317da4
conda act pt <vscode@163.com>
2020-07-24 10:46:17 +0800

    yaw

--- FaceTrackingTello.py
+++ FaceTrackingTello.py
@@ -4,7 +4,10 @@ import cv2
 global FR, batt
 FR = 50
 batt = ".."
-w, h = 640, 480
+w, h = 360, 240
+pid = [0.4, 0.4, 0]
+pError = 0
+startCounter = 0  # for no Flight 1   - for flight 0
 
 
 def battery(self):
@@ -25,20 +28,28 @@ myDrone = intializeTello()
 
 while True:
 
+    # Flight
+    if startCounter == 0:
+        myDrone.takeoff()
+        startCounter = 1
+
     # STEP 1
     img = telloGetFrame(myDrone, w, h)
     # STEP 2
     img, info = findFace(img)
-    print(info[0][0])
+    # print(info[0][0])
 
-    # DISPLAY IMAGE
-    cv2.putText(img, "Battery:{} UPDATE {}".format(battery(myDrone), str(
-        FR)), (20, 50), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 255), 2)
-    cv2.imshow("MyResult", img)
+    # Step 3
+    pError = trackFace(myDrone, info, w, pid, pError)
+    # print(info[0][0])
+
     # print(myDrone.get_battery())
     # print(FR)
 
-    # WAIT FOR THE 'Q' BUTTON TO STOP
-    if cv2.waitKey(1) and 0xFF == ord('q'):  # replace the 'and' with '&amp;'
+    # DISPLAY IMAGE
+    cv2.putText(img, "Battery:{} UPDATE {}".format(battery(myDrone), str(
+        FR)), (20, 15), cv2.FONT_HERSHEY_COMPLEX, 0.4, (0, 0, 255), 1)
+    cv2.imshow('Image', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         myDrone.land()
         break
